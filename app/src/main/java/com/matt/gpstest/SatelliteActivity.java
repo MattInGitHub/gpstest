f3c0567a2bbeca0d9c5bc880b2835ada73e2659d
MattInGitHub <513078556@qq.com>
2016-11-09 17:00:17 +0800

    更新

--- app/src/main/java/com/matt/gpstest/SatelliteActivity.java
+++ app/src/main/java/com/matt/gpstest/SatelliteActivity.java
@@ -1,26 +1,127 @@
 package com.matt.gpstest;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class SatelliteActivity extends Activity {
+    Context mContext;
     TestView testView;
     private float[] mValues;
     private SensorManager mSensorManager;
     private Sensor mSensor;
+
+    LocationManager mLocationManager;
+    LocationListenerImpl mLocationListenerImpl;
+    GpsStatusListener mGpsStatusListener;
+
+    private class GpsStatusListener implements GpsStatus.Listener {
+        @Override
+        public void onGpsStatusChanged(int event) {
+            if (ActivityCompat.checkSelfPermission(mContext, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            GpsStatus gpsStatus = mLocationManager.getGpsStatus(null);
+            ArrayList<GpsSatellite> mSatelliteList = new ArrayList<>();
+            if (event == GpsStatus.GPS_EVENT_SATELLITE_STATUS)
+            {
+                Log.e("AAAA","IN");
+                //获取最大的卫星数（这个只是一个预设值）
+                int maxSatellites = gpsStatus.getMaxSatellites();
+                Iterator<GpsSatellite> it = gpsStatus.getSatellites().iterator();
+                mSatelliteList.clear();
+                //记录实际的卫星数目
+                int count = 0;
+                while (it.hasNext() && count <= maxSatellites)
+                {
+                    //保存卫星的数据到一个队列，用于刷新界面
+                    GpsSatellite s = it.next();
+                    mSatelliteList.add(s);
+                    count++;
+                }
+                testView.drawSatellite(mSatelliteList);
+            }
+        }
+    }
+
+    private class LocationListenerImpl implements LocationListener {
+
+        //当设备位置发生变化时调用该方法
+        @Override
+        public void onLocationChanged(Location location) {
+            if (location != null) {
+                // 获取经度
+                double longitude = location.getLongitude();
+                // 获取纬度
+                double altitude = location.getAltitude();
+                String message = "经度为:" + longitude + "\n" + "纬度为:" + altitude;
+                Log.e("AAAA",message);
+            }
+        }
+
+        //当provider的状态发生变化时调用该方法.比如GPS从可用变为不可用.
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        //当provider被打开的瞬间调用该方法.比如用户打开GPS
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        //当provider被关闭的瞬间调用该方法.比如关闭打开GPS
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         testView = (TestView) findViewById(R.id.test_view);
-        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        this.mContext = this;
+        intLoc();
+    }
+
+    private void intLoc() {
+        mLocationManager = (LocationManager) this.getSystemService(this.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        //注册位置监听
+        mLocationListenerImpl = new LocationListenerImpl();
+        mGpsStatusListener = new GpsStatusListener();
+        mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 5, mLocationListenerImpl);
+        mLocationManager.addGpsStatusListener(mGpsStatusListener);
     }
 
     @Override
@@ -48,7 +149,7 @@ public class SatelliteActivity extends Activity {
         public void onSensorChanged(SensorEvent event) {
             mValues = event.values;
             if(mValues!=null){
-                testView.refreshView(mValues[0]);
+                testView.rotateView(mValues[0]);
             }
         }
         @Override
--- app/src/main/java/com/matt/gpstest/TestView.java
+++ app/src/main/java/com/matt/gpstest/TestView.java
@@ -7,109 +7,162 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathEffect;
+import android.graphics.Point;
+import android.location.GpsSatellite;
 import android.util.AttributeSet;
 import android.view.View;
+
+import java.util.ArrayList;
 
 //gps星历图
 public class TestView extends View {
 
-	//星图的X，y坐标
-	int originX;
-	int originY;
-	int radius;
-	//卫星画笔
-	Paint mNetPaint = null;
-	//文字画笔
-	Paint mTextPaint = null;
-	Path netPath;
-	float mValue;
+    //星图的X，y坐标
+    int originX;
+    int originY;
+    int radius;
+    //卫星点半径
+    float PI = 3.14159f;
+    //卫星点
+    ArrayList<GpsSatellite> satellitesList;
 
-	public TestView(Context context) {
-		this(context,null);
-	}
-	public TestView(Context context, AttributeSet attrs) {
-		this(context, attrs,0);
-	}
-	public TestView(Context context, AttributeSet attrs, int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		initView(context);
-	}
+    //圆环画笔
+    Paint mCirclePaint = null;
+    //网图画笔
+    Paint mNetPaint = null;
+    //文字画笔
+    Paint mTextPaint = null;
+    //卫星画笔
+    Paint mPointPaint = null;
+    //网图路径
+    Path netPath;
+    //旋转角度
+    float mValue;
 
-	@Override
-	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		//最终尺寸
-		int width = 0;
-		int height = 0;
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
-		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
-		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
-		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+    public TestView(Context context) {
+        this(context, null);
+    }
 
-		if (widthMode == MeasureSpec.EXACTLY) {
-			width = widthSize;
-		} else {
-			width = widthSize;
-		}
-		if (heightMode == MeasureSpec.EXACTLY) {
-			height = heightSize;
-		} else {
-			height = heightSize;
-		}
-		int Diameter = height<=width?height:width;//圆形图表的半径
-		radius = Diameter/2;
-		originX = radius;
-		originY =radius;
-		setMeasuredDimension(Diameter, Diameter);
-	}
+    public TestView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
 
+    public TestView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        //最终尺寸
+        int width = 0;
+        int height = 0;
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
 
-
-	private void initView(Context context) {
-		mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mTextPaint.setTextSize(100);
-		mTextPaint.setStyle(Paint.Style.STROKE);
-		mTextPaint.setStrokeWidth(2);
-		mTextPaint.setColor(Color.BLACK);
-
-		mNetPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-		mNetPaint.setStyle(Paint.Style.STROKE);
-		mNetPaint.setColor(Color.RED);
-		mNetPaint.setStrokeWidth(2);
-		PathEffect effects = new DashPathEffect(new float[] { 5, 5 }, 1);
-		mNetPaint.setPathEffect(effects);
-	}
-
-	public void refreshView(float SensorValue){
-		this.mValue = SensorValue;
-		invalidate();
-	}
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else {
+            width = widthSize;
+        }
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else {
+            height = heightSize;
+        }
+        int square = height <= width ? height : width;//圆形图表的半径
+        int Diameter = square - 10;//圆形图表的半径
+        radius = Diameter / 2;
+        originX = square / 2;
+        originY = square / 2;
+        setMeasuredDimension(square, square);
+    }
 
 
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		canvas.translate(originX,originY);
-		canvas.rotate(-mValue);
+    private void initView(Context context) {
+        mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mTextPaint.setTextSize(30);
+        mTextPaint.setStyle(Paint.Style.STROKE);
+        mTextPaint.setStrokeWidth(1);
+        mTextPaint.setColor(Color.WHITE);
 
-		canvas.drawCircle(0,0 , radius, mNetPaint);
+        mNetPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mNetPaint.setStrokeWidth((float) 0.8);
+        mNetPaint.setStyle(Paint.Style.STROKE);
+        mNetPaint.setColor(0xffffffff);
+        PathEffect effects = new DashPathEffect(new float[]{6, 6}, 1);
+        mNetPaint.setPathEffect(effects);
 
-		for(int i = 1;i<3;i++){
-			canvas.drawCircle(0,0, (float) ((i/3.00)*radius), mNetPaint);
-		}
+        mCirclePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mCirclePaint.setStyle(Paint.Style.FILL);
+        mCirclePaint.setColor(0xff000040);
 
-		canvas.drawText("Cool",-100,45, mTextPaint);
+        mPointPaint= new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPointPaint.setStyle(Paint.Style.STROKE);
+        mPointPaint.setColor(Color.RED);
+        mPointPaint.setStrokeWidth(2);
+    }
 
-		netPath = new Path();
-		netPath.reset();
-		for(int i = 1;i<13;i++){
-			double x = radius*Math.cos(Math.toRadians(30*i));
-			double y = radius*Math.sin(Math.toRadians(30*i));
-			netPath.moveTo((float)x,(float)y);
-			netPath.lineTo(0,0);
-		}
-		netPath.close();
-		canvas.drawPath(netPath, mNetPaint);
-	}
+    public void rotateView(float SensorValue) {
+        this.mValue = SensorValue;
+        invalidate();
+    }
+
+    public void drawSatellite(ArrayList<GpsSatellite> satellitesList) {
+        this.satellitesList =satellitesList;
+        invalidate();
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.translate(originX, originY);
+        canvas.rotate(-mValue);
+
+        canvas.drawCircle(0, 0, radius, mCirclePaint);
+
+        for (int i = 1; i < 4; i++) {
+            canvas.drawCircle(0, 0, (float) ((i / 3.00) * radius), mNetPaint);
+        }
+
+        netPath = new Path();
+        netPath.reset();
+        for (int i = 1; i < 13; i++) {
+            double x = radius * Math.cos(Math.toRadians(30 * i));
+            double y = radius * Math.sin(Math.toRadians(30 * i));
+            netPath.moveTo((float) x, (float) y);
+            netPath.lineTo(0, 0);
+        }
+        netPath.close();
+        canvas.drawPath(netPath, mNetPaint);
+
+        canvas.drawText("N", -10, -150, mTextPaint);
+        canvas.drawText("180°", -10, 150, mTextPaint);
+        canvas.drawText("270°", -200, 10, mTextPaint);
+        canvas.drawText("90°", 150, 10, mTextPaint);
+
+
+
+        if(this.satellitesList == null){
+            return;
+        }
+        int num = this.satellitesList.size();
+        if(num>=9){
+            num = 9;
+        }
+        Point satellitePos[] =new Point[10];
+        //根据方向角和高度角计算出，卫星显示的位置
+        for(int i=0;i<num;i++){
+            satellitePos[i]=new Point(0,0);
+            double dRadius = Math.cos(this.satellitesList.get(i).getElevation() * PI / 180) * radius ;
+            satellitePos[i].x = (int)(Math.sin(this.satellitesList.get(i).getAzimuth() * PI / 180)*dRadius);
+            satellitePos[i].y = -(int)(Math.cos(this.satellitesList.get(i).getAzimuth() * PI / 180)*dRadius);
+            canvas.drawText(""+this.satellitesList.get(i).getPrn(),satellitePos[i].x,satellitePos[i].y , mTextPaint);
+            canvas.drawCircle(satellitePos[i].x,satellitePos[i].y , 8,mPointPaint);
+        }
+    }
 }
